b77e02fac3f17e1580e967d7816a5f1994458aa2
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 19:53:15 -0400

    create model spec for Invitation

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Invitation do
+  it 'should save a valid invitation' do
+    expect(FactoryGirl.create(:invitation)).to be_valid
+  end
+
+  it 'should not save an invitation without a sender or recipient' do
+    expect(FactoryGirl.create(:invitation, sender_id: nil)).to_not be_valid
+    expect(FactoryGirl.create(:invitation, recipient_id: nil)).to_not be_valid
+  end
+
+  it 'should respond to sender' do
+    expect(FactoryGirl.create(:invitation)).to respond_to :sender
+  end
+
+  it 'should respond to recipient' do
+    expect(FactoryGirl.create(:invitation)).to respond_to :recipient
+  end
+
+  it 'should default to seen:false' do
+    expect(FactoryGirl.create(:invitation).seen).to eq(false)
+  end
 end
