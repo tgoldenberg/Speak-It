03a85a3be56613a0ef72eaddb4c913cc0a838b51
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 19:59:30 -0400

    create model spec for Feedback

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Feedback do
+  it 'should save a valid feedback' do
+    expect(FactoryGirl.create(:feedback)).to be_valid
+  end
+
+  it 'should not save a feedback without a rating' do
+    expect(FactoryGirl.create(:feedback, rating: nil)).to_not be_valid
+  end
+
+  it 'should respond to sender' do
+    expect(FactoryGirl.create(:feedback)).to respond_to :sender
+  end
+
+  it 'should respond to recipient' do
+    expect(FactoryGirl.create(:feedback)).to respond_to :recipient
+  end
+
+  it 'should respond to chat' do
+    expect(FactoryGirl.create(:feedback)).to respond_to :chat
+  end  
 end
