2550538ad9ddc71393bd227e64acd857a4b035d7
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 18:21:43 -0400

    add a languages factory

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -1,8 +1,13 @@
+require 'faker'
+
+## choose either English or Spanish randomly
+LANGUAGES = [{name: "English", code: "en"}, {name: "Spanish", code: "es"}]
+language = LANGUAGES.sample
+
 FactoryGirl.define do
   factory :language do
-    name "MyString"
-code "MyString"
-img_url "MyString"
+    name language.name
+    code language.code
+    img_url Faker::Avatar.image
   end
-
 end
