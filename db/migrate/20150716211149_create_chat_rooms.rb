b55940d77164e0db4049379e56014322f274d357
Alex Satterly <alcsatt@gmail.com>
2015-07-16 17:29:15 -0400

    Add constraints to chat rooms migration

--- db/migrate/20150716211149_create_chat_rooms.rb
+++ db/migrate/20150716211149_create_chat_rooms.rb
@@ -1,8 +1,8 @@
 class CreateChatRooms < ActiveRecord::Migration
   def change
     create_table :chat_rooms do |t|
-      t.references :sender
-      t.references :recipient
+      t.references :sender, index: true, foreign_key: true
+      t.references :recipient, index: true, foreign_key: true
 
       t.timestamps null: false
     end
