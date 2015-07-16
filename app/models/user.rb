635c443206bc7f5c3a65c7bdf8c61c0c4d73f2eb
Alex Satterly <alcsatt@gmail.com>
2015-07-16 19:01:18 -0400

    Add associations and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  belongs_to :country
+  belongs_to :native_language, class_name: "Language"
+  belongs_to :study_language, class_name: "Language"
+  belongs_to :level
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
