0105b5206ae0a8d2407cc6493d70cad0da22f827
Alex Satterly <alcsatt@gmail.com>
2015-07-16 17:27:34 -0400

    Add constraints to languages migration

--- db/migrate/20150716210217_create_languages.rb
+++ db/migrate/20150716210217_create_languages.rb
@@ -1,8 +1,8 @@
 class CreateLanguages < ActiveRecord::Migration
   def change
     create_table :languages do |t|
-      t.string :name
-      t.string :code
+      t.string :name, null: false, uniqueness: true
+      t.string :code, null: false, uniqueness: true
       t.string :img_url
 
       t.timestamps null: false
