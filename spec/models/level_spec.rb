beaafe139cf507eb18ba83a0f5642ec0036553dc
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-17 09:57:21 -0400

    clean up model tests to give clear error messages

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 describe Level do
+  let(:language) {FactoryGirl.create(:language)}
   it 'should save a valid level' do
-    expect(FactoryGirl.create(:level)).to be_valid
+    expect(FactoryGirl.create(:level, language_id: language.id)).to be_valid
   end
 
   it 'should respond to name and valud' do
-    expect(FactoryGirl.create(:level)).to respond_to :name
-    expect(FactoryGirl.create(:level)).to respond_to :value
+    expect(FactoryGirl.create(:level, language_id: language.id)).to respond_to :name
+    expect(FactoryGirl.create(:level, language_id: language.id)).to respond_to :value
   end
 
   it 'should respond to language' do
-    expect(FactoryGirl.create(:level)).to respond_to :language
+    expect(FactoryGirl.create(:level, language_id: language.id)).to respond_to :language
   end
 
   it 'should respond to topics' do
-    expect(FactoryGirl.create(:level)).to respond_to :topics
+    expect(FactoryGirl.create(:level, language_id: language.id)).to respond_to :topics
   end
 
   it 'should respond to users' do
-    expect(FactoryGirl.create(:level)).to respond_to :users
+    expect(FactoryGirl.create(:level, language_id: language.id)).to respond_to :users
   end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,27 +1,30 @@
 require 'rails_helper'
 
 describe Topic do
+  let(:language) {FactoryGirl.create(:language)}
+  let(:level) {FactoryGirl.create(:level, language_id: language.id)}
+  let(:topic) {FactoryGirl.create(:topic, level_id: level.id, language_id: language.id)}
   it 'should save a valid topic' do
-    expect(FactoryGirl.create(:topic)).to be_valid
+    expect(topic).to be_valid
   end
 
   it 'should respond to name' do
-    expect(FactoryGirl.create(:topic)).to respond_to :name
+    expect(topic).to respond_to :name
   end
 
   it 'should respond to level' do
-    expect(FactoryGirl.create(:topic)).to respond_to :level
+    expect(topic).to respond_to :level
   end
 
   it 'should respond to language' do
-    expect(FactoryGirl.create(:topic)).to respond_to :language
+    expect(topic).to respond_to :language
   end
 
   it 'should respond to chats' do
-    expect(FactoryGirl.create(:topic)).to respond_to :chats
+    expect(topic).to respond_to :chats
   end
 
   it 'should not save without a name' do
-    expect(FactoryGirl.create(:topic, name: nil)).to_not be_valid
+    expect(FactoryGirl.build(:topic, name: nil)).to_not be_valid
   end
 end
