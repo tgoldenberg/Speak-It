0e7c3b000eecfbc2429a770205278b396255914b
Alex Satterly <alcsatt@gmail.com>
2015-07-17 17:12:58 -0400

    Fix spelling on received invitations association

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   	if logged_in?
   	  @user = current_user
       @invitation = Invitation.new
+      #@active_invitations =
   	  @available_users = User.all.includes(:level, :country, :native_language, :study_language)
   	else
 	  	@user = User.new
