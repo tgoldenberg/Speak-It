ef36cb89faba19d1b73f2e356775e8940d5035ab
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 20:00:30 -0400

    create model spec for Country

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Country do
+  it 'should save a valid country' do
+    expect(FactoryGirl.create(:country)).to be_valid
+  end
 end
