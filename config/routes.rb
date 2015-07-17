843644539e2a2bbf5378cdd6109474b252d11413
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 20:10:54 -0400

    create a sessions controller with named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create', as: 'create_session'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
+
   root 'welcome#index'
 end
