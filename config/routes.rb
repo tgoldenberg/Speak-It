c95053555c85aa8ca5b02d668b203e499b4f98bb
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-16 20:15:40 -0400

    create Users Controller with view files and preliminary controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create', as: 'create_session'
   delete '/logout' => 'sessions#destroy', as: 'logout'
