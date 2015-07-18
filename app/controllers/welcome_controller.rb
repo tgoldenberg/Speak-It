b0c681c19ead400d9a73274f31826cf48fc0fd14
Alex Satterly <alcsatt@gmail.com>
2015-07-18 12:16:18 -0400

    Can now collect recent users that are available

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,13 @@ class WelcomeController < ApplicationController
   	  # @available_users = User.all.includes(:level, :country, :native_language, :study_language)
      @available_users =  User.all.includes(:level, :country, :native_language,
       :study_language).where("last_seen_at > ? and id != ?", 10.minutes.ago, current_user.id)
+     @recent_users = []
+     @available_users.each do |user|
+        chat = Chat.find_by(native_speaker_id: current_user.id, student_id: user.id)
+        if (chat && chat.created_at > 1.weeks.ago)
+          @recent_users << user
+        end
+     end
   	else
 	  	@user = User.new
   	end
