4458ad4dda2c2ec163fbbd6e5630c8d86590b729
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-18 17:49:49 -0400

    change state change to within AJAX done function, remove unnecessary HTML filler

--- app/assets/javascripts/components/info_panels/InfoPanel.jsx
+++ app/assets/javascripts/components/info_panels/InfoPanel.jsx
@@ -2,8 +2,7 @@ var InfoPanel = React.createClass({
   render: function() {
     return (
       <div className="infoPanel">
-        <Title chat={this.props.chat} />
-        <h4>Info Panel</h4>
+        <Title chat={this.props.chat} /><br/>
       </div>
     );
   }
