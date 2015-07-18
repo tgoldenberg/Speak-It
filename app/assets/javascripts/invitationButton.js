939a772515422dfbf1b7257f1d3079de3e443c93
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-17 23:11:45 -0400

    add event listener in layout for create chat room Pusher event and change window.location to the chat rooms#show page

--- app/assets/javascripts/invitationButton.js
+++ app/assets/javascripts/invitationButton.js
@@ -1,9 +1,11 @@
+// function for listening to a button click in order to delete an invitation
 var deleteInvitation = function() {
   $('.delete_invitation').on('click', function(e) {
     console.log("CLICK");
     var target = $(e.target);
     e.preventDefault();
     var invitationId = parseInt($(this).data('id'));
+    // call to the invitations#destroy action
     $.ajax({
       url: '/invitations',
       method: 'delete',
@@ -11,6 +13,7 @@ var deleteInvitation = function() {
       data: {invitation: {id: invitationId}}
     })
     .done(function(data) {
+      // hide the <li> tag with the buttons
       console.log(data);
       console.log(target);
       target.parent().hide();
