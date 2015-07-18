7aa6208dbc23ea47d7c993ea373154d9b8c3a293
Alex Satterly <alcsatt@gmail.com>
2015-07-17 20:16:37 -0400

    Add query to fetch active unseen invitations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   	if logged_in?
   	  @user = current_user
       @invitation = Invitation.new
-      #@active_invitations =
+      @active_invitations = @user.received_invitations.where(seen: false).pluck('DISTINCT sender_id').map{|sender_id| Invitation.find_by(recipient_id: @user.id, sender_id: sender_id) }.reverse
   	  @available_users = User.all.includes(:level, :country, :native_language, :study_language)
   	else
 	  	@user = User.new
