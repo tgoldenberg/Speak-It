83dbe5951b364ff66c13f28dc2b130b040b5853f
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-18 15:03:01 -0400

    add turn and completed fields to chat_room model in database

--- db/migrate/20150716211149_create_chat_rooms.rb
+++ db/migrate/20150716211149_create_chat_rooms.rb
@@ -3,6 +3,8 @@ class CreateChatRooms < ActiveRecord::Migration
     create_table :chat_rooms do |t|
       t.integer :creator_id
       t.integer :invitee_id
+      t.integer :turn, default: 0
+      t.boolean :completed, default: false
 
       t.timestamps null: false
     end
