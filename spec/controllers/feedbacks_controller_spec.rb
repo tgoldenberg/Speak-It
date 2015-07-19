9d24093c7b2b8178fb8ae0e35cad3729f4db3ec8
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-19 14:49:06 -0400

    create controller spec for feedbacks controller

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe FeedbacksController, type: :controller do
+  before(:each) do
+    @chat_room = ChatRoom.create(creator_id: User.first.id, invitee_id: User.last.id)
+    @first_chat = Chat.create(student_id: @chat_room.creator_id,
+      native_speaker_id: @chat_room.invitee_id,
+      topic_id: @chat_room.invitee.topics.sample.id, chat_room_id: @chat_room.id)
+    @second_chat = Chat.create(student_id: @chat_room.invitee_id,
+      native_speaker_id: @chat_room.creator_id,
+      topic_id: @chat_room.creator.topics.sample.id, chat_room_id: @chat_room.id)
+    @chat_room.chats << @first_chat << @second_chat
+
+  end
   describe 'GET new' do
     it 'should present a form for a new feedback' do
-      get :new
+      get :new, {chat_room_id: @chat_room.id}
       expect(assigns(:feedback)).to be_a Feedback
     end
   end
@@ -11,13 +22,13 @@ describe FeedbacksController, type: :controller do
   describe 'POST create' do
     it 'should create a new feedback' do
       expect {
-        post :create, {feedback: FactoryGirl.attributes_for(:feedback)}
+        post :create, {feedback: {rating: 3, comment: "Hello world", chat_id: @chat_room.chats.first.id, sender_id: @chat_room.creator_id, recipient_id: @chat_room.invitee_id }}
       }.to change(Feedback, :count).by(1)
     end
 
     it 'should not create a new feedback with invalid params' do
       expect {
-        post :create, {feedback: FactoryGirl.attributes_for(:feedback, sender_id: nil)}
+        post :create, {feedback: {rating: nil, comment: nil, chat_id: @chat_room.chats.first.id, sender_id: @chat_room.creator_id, recipient_id: @chat_room.invitee_id}}
       }.to change(Feedback, :count).by(0)
     end
   end
