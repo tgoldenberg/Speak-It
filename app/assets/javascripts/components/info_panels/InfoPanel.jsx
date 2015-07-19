1920378cbac4fcd62a244ce19b1fd7a7d494d2a6
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-19 00:11:47 -0400

    add initial guidelines component and second chat guidelines component

--- app/assets/javascripts/components/info_panels/InfoPanel.jsx
+++ app/assets/javascripts/components/info_panels/InfoPanel.jsx
@@ -1,8 +1,15 @@
 var InfoPanel = React.createClass({
   render: function() {
+    var guidelines = <div></div>;
+    if (this.props.first == true) {
+      guidelines = <InitialGuidelines/>;
+    } else {
+      guidelines = <SecondChatGuidelines />;
+    }
     return (
       <div className="infoPanel">
         <Title chat={this.props.chat} /><br/>
+        {guidelines}
       </div>
     );
   }
