44b66997c5e3eb71dca516565420b5df5c563ca5
Alex Satterly <alcsatt@gmail.com>
2015-07-20 16:52:17 -0400

    Remove tests for actions that do not exist anymore

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,43 +24,7 @@ RSpec.describe UsersController, type: :controller do
 
     it 'should not register a user with invalid params' do
       post :create, {user: FactoryGirl.attributes_for(:user, email: nil, level_id: @level.id, native_language_id: @native_language.id, study_language_id: @study_language.id, country_id: @country.id)}
-      expect(flash[:alert]).to eq(["A user with that name or email already exists"])
-    end
-  end
-
-  describe "GET #edit" do
-    it "allows the user to edit their profile" do
-      get :edit, id: @registered_user.id
-      expect(assigns(:user)).to be_a User
-    end
-  end
-
-  describe "PUT #update" do
-    it "allows the user to update their account" do
-      put :update, {id: @registered_user.id, user: FactoryGirl.attributes_for(:user, username: "Thomas", level_id: @level.id, native_language_id: @native_language.id, study_language_id: @study_language.id, country_id: @country.id) }
-      expect(User.find(@registered_user.id).username).to eq("Thomas")
-    end
-  end
-
-  describe "GET #show" do
-    it "shows the user profile" do
-      get :show, {id: @registered_user.id}
-      expect(assigns(:user).username).to eq(@registered_user.username)
-    end
-  end
-
-  describe "GET #index" do
-    it "shows all users" do
-      get :index
-      expect(assigns(:users)).to eq(User.all)
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "deletes the user account" do
-      expect{
-        delete :destroy, id: @registered_user.id
-      }.to change(User, :count).by(-1)
+      expect(flash[:alert]).to eq(["A user with that username or email already exists"])
     end
   end
 
