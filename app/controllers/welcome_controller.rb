f4103a41aee84f5519747b045e4d64e97f184d82
Alex Satterly <alcsatt@gmail.com>
2015-07-20 16:20:11 -0400

    Utilize helper to find recent users, extracted to user model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,21 +6,8 @@ class WelcomeController < ApplicationController
       @invitation = Invitation.new
       @active_invitations = User.get_active_invitations(@user)
       @available_users = User.get_available_users(@user)
-      unavailable_users = User.get_unavailable_users(@user)
-      @available_recent_users = []
-      @unavailable_recent_users = []
-      @available_users.each do |user|
-        chat = Chat.find_by(native_speaker_id: current_user.id, student_id: user.id)
-        if (chat && chat.created_at > 1.weeks.ago)
-          @available_recent_users << user
-        end
-      end
-      unavailable_users.each do |user|
-        chat = Chat.find_by(native_speaker_id: current_user.id, student_id: user.id)
-        if (chat && chat.created_at > 1.weeks.ago)
-          @unavailable_recent_users << user
-        end
-      end
+      @available_recent_users = User.find_recent_users(@available_users, @user)
+      @unavailable_recent_users = User.find_recent_users(User.get_unavailable_users(@user), @user)
   	else
 	  	@user = User.new
   	end
