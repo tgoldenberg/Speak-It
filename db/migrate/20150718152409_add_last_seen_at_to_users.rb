889643206e900799b1c31f06733c88c246e3ffca
Alex Satterly <alcsatt@gmail.com>
2015-07-20 14:41:52 -0400

    Set a default for last_seen_at

--- db/migrate/20150718152409_add_last_seen_at_to_users.rb
+++ db/migrate/20150718152409_add_last_seen_at_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastSeenAtToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :last_seen_at, :datetime
+    add_column :users, :last_seen_at, :datetime, default: 5.minutes.ago
   end
 end
