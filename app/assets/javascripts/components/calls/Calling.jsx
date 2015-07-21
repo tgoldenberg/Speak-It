2c454ae24a899c453f9533e74526da4c3824d5fa
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-21 11:24:58 -0400

    add box to hold call information

--- app/assets/javascripts/components/calls/Calling.jsx
+++ app/assets/javascripts/components/calls/Calling.jsx
@@ -1,8 +1,22 @@
 var Calling = React.createClass({
+  getInitialState: function() {
+    return {timer: 0};
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  tick: function() {
+    this.setState({timer: this.state.timer + 1});
+  },
   render: function() {
     return (
       <div className="call-box">
-        
+        <p className="calling-info">Calling {this.props.username}...</p>
+        <img src={this.props.avatar_url} className="call-avatar"/>
+        <div className="call-timer-phone">
+          <span className="glyphicon glyphicon-earphone"></span>
+          <span className="call-timer">{this.state.timer}</span>
+        </div>
       </div>
     );
   }
