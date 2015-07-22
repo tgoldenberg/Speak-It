50ac1282728be9a473156e40b964634920b3d020
Alex Satterly <alcsatt@gmail.com>
2015-07-22 12:15:43 -0400

    Add helper to generate a random conversation topic

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,8 @@ class Invitation < ActiveRecord::Base
   validates_presence_of :sender_id, :recipient_id
   belongs_to :sender, class_name: "User"
   belongs_to :recipient, class_name: "User"
+
+  def self.generate_random_topic(student)
+    student.topics.sample
+  end
 end
