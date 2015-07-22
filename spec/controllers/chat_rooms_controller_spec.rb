06341294b449b254582935c52033e018b018cf24
Alex Satterly <alcsatt@gmail.com>
2015-07-22 16:04:20 -0400

    Mock a logged in user

--- spec/controllers/chat_rooms_controller_spec.rb
+++ spec/controllers/chat_rooms_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ChatRoomsController, type: :controller do
   before(:each) do
     @invitation = Invitation.create(sender_id: User.first.id, recipient_id: User.last.id)
+    session[:user_id] = User.first.id
   end
   describe "POST #create" do
     it 'should create a new chat room with valid params' do
