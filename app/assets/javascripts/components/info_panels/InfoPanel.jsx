f26325dd7d4612138ccbc5e4c532ef8427812044
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-27 08:02:51 -0400

    hook in sub-components to passed in text data from ChatRoom component and render internationalized text

--- app/assets/javascripts/components/info_panels/InfoPanel.jsx
+++ app/assets/javascripts/components/info_panels/InfoPanel.jsx
@@ -2,9 +2,9 @@ var InfoPanel = React.createClass({
   render: function() {
     var guidelines = <div></div>;
     if (this.props.first == true) {
-      guidelines = <InitialGuidelines/>;
+      guidelines = <InitialGuidelines text={this.props.text}/>;
     } else {
-      guidelines = <SecondChatGuidelines />;
+      guidelines = <SecondChatGuidelines text={this.props.text} />;
     }
     return (
       <div>{guidelines}</div>
--- app/assets/javascripts/components/info_panels/SecondChatGuidelines.jsx
+++ app/assets/javascripts/components/info_panels/SecondChatGuidelines.jsx
@@ -3,10 +3,10 @@ var SecondChatGuidelines = React.createClass({
     return (
       <div className="container">
         <div className="well">
-          <h1>Guidelines for Second Chat</h1>
+          <h1>{this.props.text.title}</h1>
         </div>
         <div className="guidelines col-md-8 col-md-offset-2">
-          <h4>Alright, good job with the first conversation! Now your roles will switch. The student will become the native speaker, and the native speaker will become the student. Get ready for the next conversation topic, and best of luck!</h4>
+          <h4>{this.props.text.description}</h4>
         </div>
       </div>
     );
