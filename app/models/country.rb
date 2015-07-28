dd400d7391af2cf1b914ec3dc8a938f28be3f903
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-28 12:48:02 -0400

    add internationalization to country list in signup form

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,7 @@ class Country < ActiveRecord::Base
 
   def self.countries_as_select_list
     Country.order(:name).pluck(:name, :id).map do |country|
-      [I18n.t('countries.' + country[0].split(" ").join(""), country[1])]
+      [I18n.t('countries.' + country[0].split(" ").join("")), country[1]]
     end
   end
 end
