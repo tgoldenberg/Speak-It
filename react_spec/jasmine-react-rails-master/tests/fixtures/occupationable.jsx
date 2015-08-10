e50e16e661a6a9441cae16ebf7efc7618dd8310e
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-10 10:45:45 -0400

    configured Rails project to test React components with jasmine-react-rails package and gruntfile

---
+++ react_spec/jasmine-react-rails-master/tests/fixtures/occupationable.jsx
@@ -0,0 +1,290 @@
+var props = {
+  chat_room: {
+    completed: false,
+    created_at: "2015-08-10T14:04:16.256Z",
+    creator_id: 146,
+    id: 55,
+    invitee_id: 147,
+    turn: 0,
+    updated_at: "2015-08-10T14:04:16.256Z"
+  },
+  chats: [
+    {
+      chat_room_id: 55,
+      created_at: "2015-08-10T14:04:16.527Z",
+      id: 109,
+      native_speaker_id: 146,
+      student_id: 147,
+      topic_id: 104,
+      updated_at: "2015-08-10T14:04:16.527Z"
+    },
+    {
+      chat_room_id: 55,
+      created_at: "2015-08-10T14:04:16.533Z",
+      id: 110,
+      native_speaker_id: 147,
+      student_id: 146,
+      topic_id: 5,
+      updated_at: "2015-08-10T14:04:16.533Z"
+    }
+  ],
+  current_user: {
+    country: {
+      created_at: "2015-08-03T19:19:38.764Z",
+      id: 1,
+      image_url: "american-flag-medium.png",
+      name: "United States",
+      updated_at: "2015-08-03T19:19:38.764Z"
+    },
+    country_image: "/assets/american-flag-medium-ffc6ff0f18e62f073102fd8438fbc46aeafff49",
+    language: {
+      code: "en",
+      created_at: "2015-08-03T19:19:38.823Z",
+      id: 1,
+      img_url: "https://raw.githubusercontent.com/tgoldenberg/Speakit-Static/master/app/assets/images/english.jpg",
+      name: "English",
+      updated_at: "2015-08-03T19:19:38.823Z"
+    },
+    user:  {
+      avatar_url: "http://www.gravatar.com/avatar/35db3bb05d401f30e8f56885aa194e11.jpg?s=80",
+      country_id: 1,
+      created_at: "2015-08-03T19:33:35.718Z",
+      email: "alex@example.com",
+      id: 147,
+      last_seen_at: "2015-08-10T14:04:17.701Z",
+      level_id: 21,
+      native_language_id: 1,
+      password_digest: "$2a$10$45zsxDiaYpvHk5cu4oZ2Vu.a039McB3.Hnpa1Y7uCMzpsBDLP7OCO",
+      points: 0,
+      study_language_id: 3,
+      updated_at: "2015-08-10T14:04:17.708Z",
+      username: "alex"
+    }
+  },
+  final_instructions: {
+    description: "Great job everyone! Now that you've had the opportunity of practicing your language skills, make sure you thank each other for helping out in your effort to learn a new language. From here you can fill out the assessment on how your partner did. You can also suggest ways that they might improve their fluency. Make sure you make specific, actionable, and kind feedback. Take care!",
+    title: "Wrapping Up"
+  },
+  first_chat: {
+    chat: {
+      chat_room_id: 55,
+      created_at: "2015-08-10T14:04:16.527Z",
+      id: 109,
+      native_speaker_id: 146,
+      student_id: 147,
+      topic_id: 104,
+      updated_at: "2015-08-10T14:04:16.527Z"
+    },
+    language: {
+      code: "mn",
+      created_at: "2015-08-03T19:19:39.068Z",
+      id: 3,
+      img_url: "https://raw.githubusercontent.com/tgoldenberg/Speakit-Static/master/app/assets/images/chinese.png",
+      name: "Mandarin",
+      updated_at: "2015-08-03T19:19:39.068Z"
+    },
+    level: {
+      created_at: "2015-08-03T19:19:39.071Z",
+      id: 21,
+      language_id: 3,
+      name: "beginner",
+      updated_at: "2015-08-03T19:19:39.071Z",
+      value: 1
+    },
+    native_speaker: {
+      avatar_url: "http://www.gravatar.com/avatar/e5482dfaa264ed66b660327734b4ff06.jpg?s=80",
+      country_id: 18,
+      created_at: "2015-08-03T19:32:40.726Z",
+      email: "marcos@example.com",
+      id: 146,
+      last_seen_at: "2015-08-10T14:04:16.577Z",
+      level_id: 1,
+      native_language_id: 3,
+      password_digest: "$2a$10$ZI0TseeL3i0hMu5AauGnZuci3P4e5QjLcWJtiVNMEf.sJ9.PKvPZS",
+      points: 0,
+      study_language_id: 1,
+      updated_at: "2015-08-10T14:04:16.579Z",
+      username: "marcos"
+    },
+    student: {
+      avatar_url: "http://www.gravatar.com/avatar/35db3bb05d401f30e8f56885aa194e11.jpg?s=80",
+      country_id: 1,
+      created_at: "2015-08-03T19:33:35.718Z",
+      email: "alex@example.com",
+      id: 147,
+      last_seen_at: "2015-08-10T14:04:17.701Z",
+      level_id: 21,
+      native_language_id: 1,
+      password_digest: "$2a$10$45zsxDiaYpvHk5cu4oZ2Vu.a039McB3.Hnpa1Y7uCMzpsBDLP7OCO",
+      points: 0,
+      study_language_id: 3,
+      updated_at: "2015-08-10T14:04:17.708Z",
+      username: "alex"
+    },
+    text: {
+      language: "Language: Mandarin ",
+      level: "Level: (1) beginner",
+      native_speaker: "Native Speaker: Marcos",
+      student: "Student: Alex",
+      topic: "Topic: Where do you live?"
+    },
+    topic: {
+      created_at: "2015-08-03T19:19:39.078Z",
+      id: 104,
+      language_id: 3,
+      level_id: 21,
+      name: "Where do you live?",
+      updated_at: "2015-08-03T19:19:39.078Z"
+    }
+  },
+  helper_text: {
+    feedback_button: "Give Feedback",
+    seconds: "Seconds left: ",
+    stats_comments: "Comments: ",
+    stats_first_chat: "First chat: ",
+    stats_first_feedback: "First Chat Feedback",
+    stats_first_topic: "First chat topic: ",
+    stats_participants: "Participants: ",
+    stats_rating: "Rating: ",
+    stats_second_chat: "Second chat: ",
+    stats_second_feedback: "Second Chat Feedback",
+    stats_second_topic: "Second chat topic: ",
+    stats_title: "ChatRoom Stats"
+  },
+  initial_guidelines:{
+    description: "You will be given 2 minutes for each conversation. When you are the native speaker you are kindly asked not to provide instruction, but to simply carry a conversation. As a student, you are simply asked to try your best. You may use words from your native language if necessary, but will not get full credit if you do so. Native speakers are asked to judge the student's speaking skills based on pronunciation, vocabulary, and clarity.",
+    title: "Initial Guidelines"
+  },
+  other_user: {
+    country: {
+      created_at: "2015-08-03T19:19:38.803Z",
+      id: 18,
+      image_url: "chinese-flag-medium.png",
+      name: "China",
+      updated_at: "2015-08-03T19:19:38.803Z"
+    },
+    country_image: "/assets/chinese-flag-medium-04681b3a86c6ae76ed58d705a222570c3651f854",
+    language: {
+      code: "mn",
+      created_at: "2015-08-03T19:19:39.068Z",
+      id: 3,
+      img_url: "https://raw.githubusercontent.com/tgoldenberg/Speakit-Static/master/app/assets/images/chinese.png",
+      name: "Mandarin",
+      updated_at: "2015-08-03T19:19:39.068Z"
+    },
+    user: {
+      avatar_url: "http://www.gravatar.com/avatar/e5482dfaa264ed66b660327734b4ff06.jpg?s=80",
+      country_id: 18,
+      created_at: "2015-08-03T19:32:40.726Z",
+      email: "marcos@example.com",
+      id: 146,
+      last_seen_at: "2015-08-10T14:04:16.577Z",
+      level_id: 1,
+      native_language_id: 3,
+      password_digest: "$2a$10$ZI0TseeL3i0hMu5AauGnZuci3P4e5QjLcWJtiVNMEf.sJ9.PKvPZS",
+      points: 0,
+      study_language_id: 1,
+      updated_at: "2015-08-10T14:04:16.579Z",
+      username: "marcos"
+    }
+  },
+  pusher: function(appKey) {
+    this.appKey = appKey;
+  },
+  second_chat: {
+    chat: {
+      chat_room_id: 55,
+      created_at: "2015-08-10T14:04:16.533Z",
+      id: 110,
+      native_speaker_id: 147,
+      student_id: 146,
+      topic_id: 5,
+      updated_at: "2015-08-10T14:04:16.533Z"
+    },
+    language: {
+      code: "en",
+      created_at: "2015-08-03T19:19:38.823Z",
+      id: 1,
+      img_url: "https://raw.githubusercontent.com/tgoldenberg/Speakit-Static/master/app/assets/images/english.jpg",
+      name: "English",
+      updated_at: "2015-08-03T19:19:38.823Z"
+    },
+    level: {
+      created_at: "2015-08-03T19:19:38.838Z",
+      id: 1,
+      language_id: 1,
+      name: "beginner",
+      updated_at: "2015-08-03T19:19:38.838Z",
+      value: 1
+    },
+    native_speaker: {
+      avatar_url: "http://www.gravatar.com/avatar/35db3bb05d401f30e8f56885aa194e11.jpg?s=80",
+      country_id: 1,
+      created_at: "2015-08-03T19:33:35.718Z",
+      email: "alex@example.com",
+      id: 147,
+      last_seen_at: "2015-08-10T14:04:17.701Z",
+      level_id: 21,
+      native_language_id: 1,
+      password_digest: "$2a$10$45zsxDiaYpvHk5cu4oZ2Vu.a039McB3.Hnpa1Y7uCMzpsBDLP7OCO",
+      points: 0,
+      study_language_id: 3,
+      updated_at: "2015-08-10T14:04:17.708Z",
+      username: "alex"
+    },
+    student: {
+      avatar_url: "http://www.gravatar.com/avatar/e5482dfaa264ed66b660327734b4ff06.jpg?s=80",
+      country_id: 18,
+      created_at: "2015-08-03T19:32:40.726Z",
+      email: "marcos@example.com",
+      id: 146,
+      last_seen_at: "2015-08-10T14:04:16.577Z",
+      level_id: 1,
+      native_language_id: 3,
+      password_digest: "$2a$10$ZI0TseeL3i0hMu5AauGnZuci3P4e5QjLcWJtiVNMEf.sJ9.PKvPZS",
+      points: 0,
+      study_language_id: 1,
+      updated_at: "2015-08-10T14:04:16.579Z",
+      username: "marcos"
+    },
+    text: {
+      language: "Language: English ",
+      level: "Level: (1) beginner",
+      native_speaker: "Native Speaker: Alex",
+      student: "Student: Marcos",
+      topic: "Topic: What is your favorite food?"
+    },
+    topic: {
+      created_at: "2015-08-03T19:19:38.858Z",
+      id: 5,
+      language_id: 1,
+      level_id: 1,
+      name: "What is your favorite food?",
+      updated_at: "2015-08-03T19:19:38.858Z"
+    }
+  },
+  second_chat_guidelines: {
+    description: "Alright, good job with the first conversation! Now your roles will switch. The student will become the native speaker, and the native speaker will become the student. Get ready for the next conversation topic, and best of luck!",
+    title: "Guidelines for Second Chat"
+  }
+}
+var Channel = function(channel) {
+  this.channel = channel;
+  this.bind = function(otherChannel) {
+    console.log(otherChannel);
+  }
+};
+var Pusher = function(appKey) {
+  this.appKey = appKey;
+  this.subscribe = function(channel) {
+    return new Channel(channel);
+  };
+};
+
+
+
+module.exports = {
+  props: props,
+  pusher: Pusher,
+  Channel: Channel
+};
