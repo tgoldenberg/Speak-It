ac47dd3aa9d268f21a5f927e1d532b8154a378c2
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-11 16:59:02 -0400

    fix bug with csrf error on refresh of omniauth form

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {scope: 'public_profile,email', info_fields: 'email,name,locale', image_size: 'large'}
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
