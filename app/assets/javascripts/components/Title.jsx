5010685c1b9ce6407e253456cfe5ffced9d59a13
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-13 22:26:31 -0400

    fix Mandarin bug for chat room title

--- app/assets/javascripts/components/Title.jsx
+++ app/assets/javascripts/components/Title.jsx
@@ -1,13 +1,15 @@
 var Title = React.createClass({
   render: function() {
-    var native = this.props.chat.text.native_speaker.split(": ")[0];
-    var nativeName = this.props.chat.text.native_speaker.split(": ")[1];
-    var student = this.props.chat.text.student.split(": ")[0];
-    var studentName = this.props.chat.text.student.split(": ")[1];
+    var native = this.props.chat.text.native_speaker.split(":")[0];
+    var nativeName = this.props.chat.text.native_speaker.split(":")[1];
+    var student = this.props.chat.text.student.split(":")[0];
+    var studentName = this.props.chat.text.student.split(":")[1];
     var language = this.props.chat.text.language.split(":")[0];
     var languageName = this.props.chat.text.language.split(":")[1];
-    var topic = this.props.chat.text.topic.split(": ")[0];
-    var topicName = this.props.chat.text.topic.split(": ")[1];
+    var topic = this.props.chat.text.topic.split(":")[0];
+    var topicName = this.props.chat.text.topic.split(":")[1];
+    console.log(native);
+    console.log(nativeName);
     return (
       <div>
       <div className="titleBar row">
@@ -36,12 +38,12 @@ var Title = React.createClass({
           <h2 id="title_topic"><b>{topic}:</b> {topicName}</h2>
         </div>
 
-        <div className="language-wrapper col-sm-2">
+        <div className="language-wrapper col-md-2 hidden-sm">
           <span id="title_language"><b>{language}:</b><br/>{languageName}</span>
           <img className="avatar-image hidden-sm hidden-xs" src={this.props.chat.language.img_url}/>
         </div>
 
-        <div className="level-wrapper col-md-2 hidden-sm hidden-xs">
+        <div className="level-wrapper col-lg-2 hidden-md hidden-sm hidden-xs">
           <span id="title_level">{this.props.chat.text.level}</span>
         </div>
       </div>
