f4bce257ab139692f03e67211a5c6e328a6e4485
tgoldenberg <thomasagoldenberg@gmail.com>
2015-09-01 17:13:30 -0400

    debug

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -30,8 +30,7 @@ class ChatRoom < ActiveRecord::Base
           user: data_hash[:current_user],
           country: data_hash[:current_user].try(:country),
           country_image: ActionController::Base.helpers.asset_path(data_hash[:current_user].try(:country).try(:image_url)),
-          language: data_hash[:current_user].try(:native_language),
-          language_flag: ActionController::Base.helpers.asset_path("languages/#{data_hash[:current_user].try(:language).try(:name).downcase}.png")
+          language: data_hash[:current_user].try(:native_language)
         },
         initial_guidelines: {
           title: I18n.t('chat_room.initial_guidelines_title'),
@@ -49,8 +48,7 @@ class ChatRoom < ActiveRecord::Base
           user: data_hash[:other_user],
           country: data_hash[:other_user].country,
           country_image: ActionController::Base.helpers.asset_path(data_hash[:other_user].country.image_url),
-          language: data_hash[:other_user].native_language,
-          language_flag: ActionController::Base.helpers.asset_path("languages/#{data_hash[:other_user].try(:language).try(:name).downcase}.png")
+          language: data_hash[:other_user].native_language
         },
         first_chat: {
           chat: data_hash[:first_chat],
